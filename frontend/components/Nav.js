7d6833a6ae25844bb6a27197541962c17f7fd1d5
Dan Webb <mail@danwebb.co>
2018-10-20 21:13:45 +0100

    Setup + Styles (Ep. 0 - 9)

---
+++ frontend/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
+
+const Nav = () => (
+	<NavStyles>
+		<Link href="/items"><a>items</a></Link>
+		<Link href="/sell"><a>sell</a></Link>
+		<Link href="/signup"><a>signup</a></Link>
+		<Link href="/orders"><a>orders</a></Link>
+		<Link href="/account"><a>account</a></Link>
+	</NavStyles>
+);
+
+export default Nav;
---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Home = () => (
+	<div>
+		<p>Hey!</p>
+	</div>
+);
+
+export default Home;
