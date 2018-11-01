f4942f750b0ea87845db6a2add498feed5834f87
Dan Webb <mail@danwebb.co>
2018-11-01 10:14:47 +0000

    Deleting items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -10,6 +10,12 @@ const Mutations = {
 			data: {...updates},
 			where: {id: args.id}
 		}, info);
+	},
+
+	async deleteItem(parent, args, ctx, info) {
+		const where = {id: args.id};
+		const item = await ctx.db.query.item({where}, `{id, title}`);
+		return ctx.db.mutation.deleteItem({where}, info);
 	}
 };
 
